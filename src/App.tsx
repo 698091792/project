20f17854053389d7e51550d4242d2b3c733d7eba
Infy <juniorbilogue@gmail.com>
2025-01-16 13:46:52 +0100

    one test again

--- src/App.tsx
+++ src/App.tsx
@@ -151,7 +151,7 @@ function App() {
 
       {/* Our Team */}
       <section id="team" className="py-20 bg-gray-100 px-4">
-        <div className="max-w-7xl mx-auto">
+        <div className="max-w-7xl mx-auto ">
           <h2 className="text-4xl font-bold text-center mb-12">{t('team')}</h2>
           <AutoSlideTeam />
         </div>
